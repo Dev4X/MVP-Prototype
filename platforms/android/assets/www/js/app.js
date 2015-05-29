a18962c5ab1933d0c96f9c9e4ce44dd7100b61df
Hiren Dave <hdave10@gmail.com>
2015-05-29 16:40:15 +0530

    App Launch Logic
    Native JavaScript code to launch app is added. also cordova launcher
    plugin is working.

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -1,4 +1,4 @@
-alert("Alert!")
+//alert("Alert!")
 
 //angular.module('Dev4XProto', ['ngCordova'])
 
@@ -7,25 +7,39 @@ document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-	alert("Ready")
+	//alert("Ready")
 	if (window.cordova) {
-		alert("Cordova");
-		}
+		//alert("Cordova");
+	}
 		
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+	//There no keyboard plugin so commented this code as it was breaking app: Hiren
+    /*if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-	
-	var successCallback = function(data) {
-        alert("Success!");
-        // if calling canLaunch() with getAppList:true, data will contain an array named "appList" with the package names of applications that can handle the uri specified.
-    };
-    var errorCallback = function(errMsg) {
-        alert("Error! " + errMsg);
-    }
-	
-	window.plugins.launcher.canLaunch({packageName:'com.facebook.katana'}, successCallback, errorCallback);
+    }*/
+}
+
+var packageName = '';//var to store package name.
+
+//This function is called from html page by onclick event
+function launchApp(_packageName){
+    packageName = _packageName;
+    //first check if app is available
+    window.plugins.launcher.canLaunch({packageName: _packageName}, successCallback, errorCallback);
+}
+
+function successCallback(){
+	//launch the app with package name;
+	window.plugins.launcher.launch({packageName: packageName}, appLaunchSuccessCallback, appLaunchErrorCallback);
+}
+
+function errorCallback(){
+	alert('Application not found');
+}
+
+function appLaunchSuccessCallback(){
+	//app launched. do the next steps
+}
+
+function appLaunchErrorCallback(){
+	alert('Error in launching app');
 }
